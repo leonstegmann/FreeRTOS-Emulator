1d95be68b53f935943cee1bbe54ed60438f9b39e
Leon Stegmann VM <leon.stegmann@tum.de>
2022-11-09 19:21:42 +0100

    UPDATE exercise_2, moving drawing function from main.c to draw.c cherry pick after update from original forked repo

--- src/main.c
+++ src/main.c
@@ -21,84 +21,13 @@
 
 #include "AsyncIO.h"
 
+#include "draw.h"
+#include "buttons.h"
+
 #define mainGENERIC_PRIORITY (tskIDLE_PRIORITY)
 #define mainGENERIC_STACK_SIZE ((unsigned short)2560)
 
-static TaskHandle_t DemoTask = NULL;
-
-typedef struct buttons_buffer {
-	unsigned char buttons[SDL_NUM_SCANCODES];
-	SemaphoreHandle_t lock;
-} buttons_buffer_t;
-
-static buttons_buffer_t buttons = { 0 };
-
-void xGetButtonInput(void)
-{
-	if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-		xQueueReceive(buttonInputQueue, &buttons.buttons, 0);
-		xSemaphoreGive(buttons.lock);
-	}
-}
-
-#define KEYCODE(CHAR) SDL_SCANCODE_##CHAR
-
-void vDemoTask(void *pvParameters)
-{
-	// structure to store time retrieved from Linux kernel
-	static struct timespec the_time;
-	static char our_time_string[100];
-	static int our_time_strings_width = 0;
-
-	// Needed such that Gfx library knows which thread controlls drawing
-	// Only one thread can call tumDrawUpdateScreen while and thread can call
-	// the drawing functions to draw objects. This is a limitation of the SDL
-	// backend.
-	tumDrawBindThread();
-
-	while (1) {
-		tumEventFetchEvents(
-			FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
-		xGetButtonInput(); // Update global input
-
-		// `buttons` is a global shared variable and as such needs to be
-		// guarded with a mutex, mutex must be obtained before accessing the
-		// resource and given back when you're finished. If the mutex is not
-		// given back then no other task can access the reseource.
-		if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-			if (buttons.buttons[KEYCODE(
-				    Q)]) { // Equiv to SDL_SCANCODE_Q
-				exit(EXIT_SUCCESS);
-			}
-			xSemaphoreGive(buttons.lock);
-		}
-
-		tumDrawClear(White); // Clear screen
-
-		clock_gettime(CLOCK_REALTIME,
-			      &the_time); // Get kernel real time
-
-		// Format our string into our char array
-		sprintf(our_time_string,
-			"There has been %ld seconds since the Epoch. Press Q to quit",
-			(long int)the_time.tv_sec);
-
-		// Get the width of the string on the screen so we can center it
-		// Returns 0 if width was successfully obtained
-		if (!tumGetTextSize((char *)our_time_string,
-				    &our_time_strings_width, NULL))
-			tumDrawText(our_time_string,
-				    SCREEN_WIDTH / 2 -
-					    our_time_strings_width / 2,
-				    SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2,
-				    TUMBlue);
-
-		tumDrawUpdateScreen(); // Refresh the screen to draw string
-
-		// Basic sleep of 1000 milliseconds
-		vTaskDelay((TickType_t)1000);
-	}
-}
+static TaskHandle_t Draw = NULL;
 
 int main(int argc, char *argv[])
 {
@@ -127,10 +56,13 @@ int main(int argc, char *argv[])
 		goto err_buttons_lock;
 	}
 
-	if (xTaskCreate(vDemoTask, "DemoTask", mainGENERIC_STACK_SIZE * 2, NULL,
-			mainGENERIC_PRIORITY, &DemoTask) != pdPASS) {
-		goto err_demotask;
-	}
+    tumDrawClear(White); // Clear screen
+
+
+    if (xTaskCreate(vDraw, "Draw_exercise_2", mainGENERIC_STACK_SIZE * 2, NULL,
+                    mainGENERIC_PRIORITY, &Draw) != pdPASS) {
+        goto err_demotask;
+    }
 
 	vTaskStartScheduler();
 
