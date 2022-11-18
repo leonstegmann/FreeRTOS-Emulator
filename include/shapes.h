281159f529e5c2cfdd490e60fc25097d30bed6b3
Leon Stegmann VM <leon.stegmann@tum.de>
2022-11-18 18:35:50 +0100

    ADD shapes.c and shapes.h for Triangle,Circle and Square

---
+++ include/shapes.h
@@ -0,0 +1,53 @@
+#ifndef __SHAPES_H__  
+#define __SHAPES_H__
+
+#include"TUM_Draw.h"
+
+typedef struct circle {
+
+    unsigned short radius; /**< Radius of the ball in pixels */
+
+    coord_t location; /**< Absolute location of circle */
+
+    float dx; /**< X axis speed in pixels/second */
+    float dy; /**< Y axis speed in pixels/second */
+
+    unsigned int colour; /**< Hex RGB colour of the ball */
+
+} circle_t;
+
+typedef struct triangle {
+
+    coord_t corners[3];
+
+    coord_t location; /**< Absolute location of circle */
+
+
+    float dx; /**< X axis speed in pixels/second */
+    float dy; /**< Y axis speed in pixels/second */
+
+    unsigned int colour; /**< Hex RGB colour of the ball */
+
+} triangle_t;
+
+typedef struct square {
+
+    unsigned short width; /**< Width of square */
+    unsigned short height; /**< Height of square */
+    
+    coord_t location; /**< Absolute location of circle */
+
+    float dx; /**< X axis speed in pixels/second */
+    float dy; /**< Y axis speed in pixels/second */
+
+    unsigned int colour; /**< Hex RGB colour of the ball */
+
+} square_t;
+
+int drawCircle(circle_t* );
+
+int drawSquare(square_t* );
+
+int drawTriangle(triangle_t* );
+
+#endif //__SHAPES_H__
---
+++ src/shapes.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include"stdlib.h"
+
+#include"TUM_Draw.h"
+
+#include"shapes.h"
+
+circle_t *createCircle(coord_t initial_location, unsigned short radius,
+                   unsigned int colour)
+{
+    circle_t *ret = calloc(1, sizeof(circle_t));
+
+    if (!ret) {
+        fprintf(stderr, "Creating ball failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret->location = initial_location;
+    ret->colour = colour;
+    ret->radius = radius;
+    ret->dx = 0;
+    ret->dy = 0;    
+
+    return ret;
+}
+
+square_t *createSquare(coord_t initial_location,  unsigned short initial_width,
+                    unsigned short initial_height, unsigned int colour)
+{
+    square_t *ret = calloc(1, sizeof(square_t));
+
+    if (!ret) {
+        fprintf(stderr, "Creating ball failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret->location = initial_location;
+    ret->colour = colour;
+    ret->width = initial_width;
+    ret->height = initial_height;
+    ret->dx = 0;
+    ret->dy = 0;    
+
+    return ret;
+}
+
+triangle_t *createTriangle(coord_t initial_location, coord_t *initial_corners[0],
+                   unsigned int colour)
+{
+    triangle_t *ret = calloc(1, sizeof(triangle_t));
+
+    if (!ret) {
+        fprintf(stderr, "Creating ball failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ret->location = initial_location;
+    ret->corners[0] = *initial_corners[0];
+    ret->corners[1] = *initial_corners[1];
+    ret->corners[2] = *initial_corners[2];
+    ret->colour = colour;
+    ret->dx = 0;
+    ret->dy = 0;    
+
+    return ret;
+}
+
+int drawCircle(circle_t *circle){
+    tumDrawCircle( circle->location.y,circle->location.y, circle->radius, circle->colour);
+    return 0;
+}
+
+int drawSquare(square_t *square){
+    tumDrawFilledBox( square->location.x - square->width/2 ,square->location.y + square->height/2, square->width, square->height, square->colour);
+    return 0;
+}
+
+int drawTriangle(triangle_t *triangle){
+    coord_t coordsCentered[3];
+    for (int i=0; i<3; i++){
+        coordsCentered[i].x = triangle->corners[i].x + SCREEN_WIDTH / 2  ;
+        coordsCentered[i].y = triangle->corners[i].y + SCREEN_HEIGHT/2 ;
+    }
+    tumDrawTriangle(coordsCentered, triangle->colour);
+    return 0;
+}
