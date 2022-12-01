1d8c5d4e78ae695277b39ec89db78103671003fc
Leon Stegmann VM <leon.stegmann@tum.de>
2022-12-01 11:29:53 +0100

    UPDATE defines.h for DrawFPS

--- include/defines.h
+++ include/defines.h
@@ -4,3 +4,5 @@
 #define KEYCODE(CHAR)           SDL_SCANCODE_##CHAR     // SDL_SCANCODE for te keybord use i buttons
 #define FRAMERATE               50
 #define SCREEN_CENTER (coord_t) {SCREEN_WIDTH/2 , SCREEN_HEIGHT/2} 
+#define FPS_AVERAGE_COUNT 50
+#define FPS_FONT "IBMPlexSans-Bold.ttf"
