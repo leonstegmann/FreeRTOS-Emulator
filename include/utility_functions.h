ae931da6fa8baeeb5a91c405af0fbbfa6259e4df
Leon Stegmann VM <leon.stegmann@tum.de>
2022-12-01 11:28:39 +0100

    ADD utility functions.c -.h for DrawFPS

---
+++ include/utility_functions.h
@@ -0,0 +1,25 @@
+#ifndef __MAIN__ 
+#define __MAIN__
+
+/* Library includes */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* TUM_Library includes  */
+#include "TUM_Ball.h"
+#include "TUM_Print.h"
+#include "TUM_Draw.h"
+#include "TUM_Event.h"
+#include "TUM_Sound.h"
+#include "TUM_Utils.h"
+#include "TUM_Font.h"
+#include "TUM_FreeRTOS_Utils.h"
+
+/* Project includes  */
+#include "defines.h"
+
+void vDrawFPS(void);
+
+#endif 
---
+++ src/utility_functions.c
@@ -0,0 +1,55 @@
+#include"utility_functions.h"
+
+void vDrawFPS(void)
+{
+    static unsigned int periods[FPS_AVERAGE_COUNT] = { 0 };
+    static unsigned int periods_total = 0;
+    static unsigned int index = 0;
+    static unsigned int average_count = 0;
+    static TickType_t xLastWakeTime = 0, prevWakeTime = 0;
+    static char str[10] = { 0 };
+    static int text_width;
+    int fps = 0;
+    font_handle_t cur_font = tumFontGetCurFontHandle();
+
+    if (average_count < FPS_AVERAGE_COUNT) {
+        average_count++;
+    }
+    else {
+        periods_total -= periods[index];
+    }
+
+    xLastWakeTime = xTaskGetTickCount();
+
+    if (prevWakeTime != xLastWakeTime) {
+        periods[index] =
+            configTICK_RATE_HZ / (xLastWakeTime - prevWakeTime);
+        prevWakeTime = xLastWakeTime;
+    }
+    else {
+        periods[index] = 0;
+    }
+
+    periods_total += periods[index];
+
+    if (index == (FPS_AVERAGE_COUNT - 1)) {
+        index = 0;
+    }
+    else {
+        index++;
+    }
+
+    fps = periods_total / average_count;
+
+    tumFontSelectFontFromName(FPS_FONT);
+
+    sprintf(str, "FPS: %2d", fps);
+
+    if (!tumGetTextSize((char *)str, &text_width, NULL))
+        tumDrawText(str, SCREEN_WIDTH - text_width - 40,
+                              SCREEN_HEIGHT - DEFAULT_FONT_SIZE - 40,
+                              Skyblue);
+
+    tumFontSelectFontFromHandle(cur_font);
+    tumFontPutFontHandle(cur_font);
+}
