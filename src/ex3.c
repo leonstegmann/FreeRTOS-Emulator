3ecf50391b41716f20e27c84b919011a150e23fb
Leon Stegmann VM <leon.stegmann@tum.de>
2022-12-07 19:22:45 +0100

    UPDATE circle blinging time and code formatting

--- src/ex3.c
+++ src/ex3.c
@@ -9,6 +9,7 @@
 #include "TUM_Ball.h"
 #include "TUM_Event.h"
 #include "TUM_Font.h"
+#include"TUM_FreeRTOS_Utils.h"
 
 /* Project includes  */
 #include"ex3.h"
@@ -35,33 +36,27 @@ void vExercise3Draw(void *pvParameters){
     circle_t *right_circle = createCircle((coord_t) {3*SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2 }, (unsigned short) 30, Red);
     int rightCircle_State;
 
-
-    tumDrawBindThread();
     
     while(1){
-        if (DrawSignal);
-        if(xSemaphoreTake(DrawSignal, 10) == pdTRUE);
-
-        tumEventFetchEvents( FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
-		xGetButtonInput(); // Update global input
-
-        tumDrawClear(White);
-
-        vDrawFPS();
-
-    	xQueueReceive(Queue_ex3_Handle,&leftCircle_State,20);
-        xQueueReceive(Queue_ex3_Handle,&rightCircle_State,20);
-		
-        if (leftCircle_State == 1)
-            drawCircle(left_circle);
+        if (DrawSignal)
+            if (xSemaphoreTake(DrawSignal, portMAX_DELAY) == pdTRUE) {
+            tumEventFetchEvents( FETCH_EVENT_NONBLOCK); // Query events backend for new events, ie. button presses
             
-        if (rightCircle_State == 1)
-            drawCircle(right_circle);
-            
-        tumDrawUpdateScreen();
-        
-        xSemaphoreGive(DrawSignal);
+            tumDrawClear(White);
 
+            vDrawFPS();
+
+            xQueueReceive(Queue_ex3_Handle,&leftCircle_State,20);
+            xQueueReceive(Queue_ex3_Handle,&rightCircle_State,20);
+            
+            if (leftCircle_State == 1)
+                drawCircle(left_circle);
+                
+            if (rightCircle_State == 1)
+                drawCircle(right_circle);
+                        
+            vTaskDelay(10);
+            }
     }
 }
 
@@ -72,11 +67,11 @@ void vExercise3Task1(void *pvParameters){
     while(1){
     leftCircle_State = 1;
     xQueueSend(Queue_ex3_Handle, &leftCircle_State, portMAX_DELAY);
-    vTaskDelay((TickType_t) 50);
+    vTaskDelay((TickType_t) 500);
 
     leftCircle_State = 0;
     xQueueSend(Queue_ex3_Handle, &leftCircle_State, portMAX_DELAY);
-    vTaskDelay((TickType_t) 50);
+    vTaskDelay((TickType_t) 500);
 
     }
 }
@@ -88,29 +83,29 @@ void vExercise3Task2(void *pvParameters){
     while(1){
     rightCircle_State = 1;
     xQueueSend(Queue_ex3_Handle, &rightCircle_State, portMAX_DELAY);
-    vTaskDelay((TickType_t) 25);
+    vTaskDelay((TickType_t) 250);
 
     rightCircle_State = 0;
     xQueueSend(Queue_ex3_Handle, &rightCircle_State, portMAX_DELAY);
-    vTaskDelay((TickType_t) 25);
+    vTaskDelay((TickType_t) 250);
 
     }
 }
 
 int createExercise3Tasks(void){
 
-    if (xTaskCreate(vExercise3Draw, "Exercise_3_Draw", mainGENERIC_STACK_SIZE * 2, NULL,
-                    mainGENERIC_PRIORITY, &Ex3_draw_handle) != pdPASS) {
+    if (xTaskCreate(vExercise3Draw, "Exercise_3_Draw", mainGENERIC_STACK_SIZE, NULL,
+                    mainGENERIC_PRIORITY+2, &Ex3_draw_handle) != pdPASS) {
 		goto err_ex3_draw;
 	}
 
-	if (xTaskCreate(vExercise3Task1, "Exercise_3_Task_1", mainGENERIC_STACK_SIZE * 2, NULL,
+	if (xTaskCreate(vExercise3Task1, "Exercise_3_Task_1", mainGENERIC_STACK_SIZE , NULL,
                     mainGENERIC_PRIORITY+1, &Ex3_t1_handle) != pdPASS) {
 		goto err_ex3_t1;
 	}
 
-	if (xTaskCreate(vExercise3Task2, "Exercise_3_Task_2", mainGENERIC_STACK_SIZE * 2, NULL,
-                    mainGENERIC_PRIORITY, &ex3_t2_handle) != pdPASS) {
+	if (xTaskCreate(vExercise3Task2, "Exercise_3_Task_2", mainGENERIC_STACK_SIZE , NULL,
+                    mainGENERIC_PRIORITY+1, &ex3_t2_handle) != pdPASS) {
 		goto err_ex3_t2;
 	}
 
@@ -144,7 +139,7 @@ void deleteExercise3Tasks(void)
 }
 
 void Exercise3ExitFunction(void){
-    printf("Exiting Tasks 3");
+    printf("\nExiting Tasks 3\n");
     vTaskSuspend(Ex3_draw_handle);
 	vTaskSuspend(Ex3_t1_handle);
 	vTaskSuspend(ex3_t2_handle);
@@ -152,11 +147,10 @@ void Exercise3ExitFunction(void){
 }
 
 void Exercise3EnterFunction(void){
-    printf("Resume Tasks 3");
+    printf("\nResume Tasks 3\n");
     vTaskResume(Ex3_draw_handle);
 	vTaskResume(Ex3_t1_handle);
 	vTaskResume(ex3_t2_handle);
-
 }
 
 
